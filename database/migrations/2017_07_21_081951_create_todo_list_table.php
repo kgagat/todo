db514123c285f2f4f70d5ee2193f7b396908c922
korzechowski <kacper.orzechowski96@gmail.com>
2017-08-04 10:48:23 +0200

    Merge branch 'master' of github.com:kgagat/todo
    # Conflicts:
    #	resources/views/todoList.blade.php

--- database/migrations/2017_07_21_081951_create_todo_list_table.php
+++ database/migrations/2017_07_21_081951_create_todo_list_table.php
@@ -13,7 +13,7 @@ class CreateTodoListTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo_list', function (Blueprint $table) {
+        Schema::create('todo_list', function ( Blueprint $table) {
             $table->increments('id');
             $table->string('description', 255);
             $table->integer('user_id')->unsigned();
@@ -29,6 +29,6 @@ class CreateTodoListTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todo_list');
+        Schema::dropIfExists( 'todo_list' );
     }
 }
